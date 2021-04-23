74c23ae4ebb31a9c240eee62f1ddb89a2e855487
mgdepetro <82244448+mgdepetro@users.noreply.github.com>
2021-04-22 20:53:03 -0400

    Changes empty values list to use rows and columns

--- xml_testing.py
+++ xml_testing.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 if __name__ == '__main__':
 	# Get element tree object
-	tree = et.parse('test puzzle specifications/3x3_02_solvable.xml')
+	tree = et.parse('3x3_02_solvable.xml')
 	
 	# Get the root element
 	root = tree.getroot()
@@ -20,8 +20,8 @@ if __name__ == '__main__':
 	# print(root.find('start_state').text)
 	
 	#XML Variables
-	rows_per_box = root.find('rows_per_box').text
-	cols_per_box = root.find('cols_per_box').text
+	rows_per_box = int(root.find('rows_per_box').text)
+	cols_per_box = int(root.find('cols_per_box').text)
 	name = root.find('name').text
 	well_formed = root.find('well_formed').text
 	solvable = root.find('solvable').text
@@ -54,9 +54,6 @@ if __name__ == '__main__':
 	puzzle = root.find('start_state').text
 	puzzle = puzzle.replace("\\", "")
 	
-	
-	
-	
 	COMBINE_WHITESPACE = re.compile(r"\s+")
 	stripped_puzzle = COMBINE_WHITESPACE.sub(" ", puzzle).strip()
 	
@@ -68,23 +65,28 @@ if __name__ == '__main__':
 	
 	full_puzzle_dict = OrderedDict()
 	
-	empty_cell_vals = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+	#Fills the empty cell values with values based on rows and columns
+	empty_cell_vals = range(1,rows_per_box*cols_per_box + 1)
+	print("This puzzles values: ")
+	for i in empty_cell_vals:
+		print(i)
 	
-	for i in range(9):
-		for j in range(9):
-			if (i, j) not in puzzle_dict.keys():
-				full_puzzle_dict[(i, j)] = empty_cell_vals
-			else:
-				full_puzzle_dict[(i, j)] = puzzle_dict[(i, j)]
+	#for i in range(9):
+	#	for j in range(9):
+	#		if (i, j) not in puzzle_dict.keys():
+	#			full_puzzle_dict[(i, j)] = empty_cell_vals
+	#		else:
+	#			full_puzzle_dict[(i, j)] = puzzle_dict[(i, j)]
 				
 				
-	for key, value in full_puzzle_dict.items():
-		print(key, value)
+	#for key, value in full_puzzle_dict.items():
+	#	print(key, value)
 		
-	print(name)
-	print(rows_per_box)
-	print(cols_per_box)
-	print(well_formed)
-	print(solvable)
-	print(unique_solution)
-	print(pigeonhole)
+	print()
+	print("puzzle name:" , name)
+	print("rows:" , rows_per_box)
+	print("columns:" , cols_per_box)
+	print("well formed? :" , well_formed)
+	print("solvable?:" , solvable)
+	print("unique solution?: " , unique_solution)
+	print("pidgeon hole?: " , pigeonhole)
